1c3ca7817e3153e548cff505e37261613a55382a
rogerpq <rogerpq@users.noreply.github.com>
2023-01-05 11:56:40 +0000

    اشهد ان عليا ولي الله

--- repthon/Config/roza_config.py
+++ repthon/Config/roza_config.py
@@ -27,7 +27,9 @@ class Var:
         if len(sys.argv) > 2
         else config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
     )
-    STRING_SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=None)
+    STRING_SESSION = (
+        sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=None)
+    )
     REDIS_URI = (
         sys.argv[4]
         if len(sys.argv) > 4
