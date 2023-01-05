827af3e5f49d7bb26a57b00495c8f8fc8f202b46
روجر <104784913+rogerpq@users.noreply.github.com>
2023-01-05 08:51:12 +0300

    Update roza_config.py

--- repthon/Config/roza_config.py
+++ repthon/Config/roza_config.py
@@ -4,6 +4,6 @@ import os
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
-    from sample_config import Config  # noqa
+    from sample_config import config  # noqa
 elif os.path.exists("config.py"):
     from config import Development as Config  # noqa
